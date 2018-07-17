7f29d9261aac31993f4f91ab326bf3ca6c62e766
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-07-17 15:21:57 +0200

    [odigo-auditor][fabric] Add fabfile.py to the project

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# Name: odigo-auditor
+
+from fabric.api import local
+
+def git():
+    """Configure Git"""
+
+    local("git remote rm origin")
+    local("git remote add origin https://korniichuk@github.com/korniichuk/odigo-auditor.git")
