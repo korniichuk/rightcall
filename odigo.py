5f12a7921ecfe7593e28b51aa0dc72058560267d
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-07-18 15:50:01 +0200

    [odigo-auditor][odigo] Add download_mp3_by_ref() function

--- odigo.py
+++ odigo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 
 import time
-from sys import exit
 
 from bs4 import BeautifulSoup
 from fbi import getpassword
@@ -20,80 +19,6 @@ s = Session(webdriver_path=driver,
             default_timeout=15,
             webdriver_options={'arguments': ['headless']})
 
-def login(s, username, passwd):
-    """Login to www.prosodie.com with username/passwd pair and return session.
-    Input:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session);
-        username -- username on www.prosodie.com (required | type: str);
-        passwd -- password for username on www.prosodie.com (required |
-                  type: str).
-    Output:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session).
-
-    """
-
-    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
-          'EntryPoint?serviceName=LoginHandler'
-    s.driver.get(url)
-    s.driver.ensure_element_by_name('mail').send_keys(username)
-    s.driver.ensure_element_by_name('password').send_keys(passwd)
-    s.driver.ensure_element_by_name('valider').click()
-    return s
-
-def search_by_range(s, start_date=None, start_time=None, end_date=None,
-                    end_time=None):
-    """Search records on www.prosodie.com by date range and return session.
-    Input:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session);
-        start_date -- start date (not required | type: str). Format:
-                      'mm:dd:yyyy'. Example: '03-05-1991';
-        start_time -- start time (not required | type: str). Example:
-                      '12:00 AM';
-        end_date -- end date (not required | type: str). Format:
-                    'mm:dd:yyyy'. Example: '03-05-1991';
-        end_time -- end time (not required | type: str). Example: '12:00 PM'.
-    Output:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session).
-
-    """
-
-    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
-          'EntryPoint?serviceName=CriteresMessagesHandler&lang=en'
-    s.driver.get(url)
-    if start_date:
-        s.driver.ensure_element_by_name('dateDebut').send_keys(start_date)
-    if start_time:
-        s.driver.ensure_element_by_name('heureDebut').send_keys(start_time)
-    if end_date:
-        s.driver.ensure_element_by_name('dateFin').send_keys(end_date)
-    if end_time:
-        s.driver.ensure_element_by_name('heureFin').send_keys(end_time)
-    s.driver.ensure_element_by_id('button-1009').click()
-    return s
-
-def search_by_ref(s, ref):
-    """Search record on www.prosodie.com by ref number and return session.
-    Input:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session);
-        ref -- ref number (required | type: str). Example: '3905beTOd10339'.
-    Output:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session).
-
-    """
-
-    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
-          'EntryPoint?serviceName=CriteresMessagesHandler&lang=en'
-    s.driver.get(url)
-    s.driver.ensure_element_by_name('refEr').send_keys(ref)
-    s.driver.ensure_element_by_id('button-1009').click()
-    return s
-
 def download_mp3(s, path=None):
     """Download .MP3 file from www.prosodie.com page and return session.
     Input:
@@ -101,8 +26,7 @@ def download_mp3(s, path=None):
              type: requestium.requestium.Session);
         path -- mp3 file absolute path (not required | type: str).
     Output:
-        s -- Requestium session (required |
-             type: requestium.requestium.Session).
+        s -- Requestium session (type: requestium.requestium.Session).
 
     """
 
@@ -130,11 +54,101 @@ def download_mp3(s, path=None):
     s.transfer_session_cookies_to_driver()
     return s
 
-### Download .MP3 file by 3905beTOd10339 ref number ###
-s = login(s, username, passwd)
-s = search_by_ref(s, '3905beTOd10339')
-result = download_mp3(s)
-if result != 1:
-   s = result
-else:
-   exit(1)
+def download_mp3_by_ref(s, username, passwd, ref, path=None):
+    """Download .MP3 file from www.prosodie.com page by ref number and return
+       session.
+    Input:
+        s -- Requestium session (required |
+             type: requestium.requestium.Session);
+        username -- username on www.prosodie.com (required | type: str);
+        passwd -- password for username on www.prosodie.com (required |
+                  type: str);
+        ref -- ref number (required | type: str). Example: '3905beTOd10339';
+        path -- mp3 file absolute path (not required | type: str).
+
+    """
+
+    s = login(s, username, passwd)
+    s = search_by_ref(s, ref)
+    result = download_mp3(s, path)
+    if result == 1:
+        return 1
+
+def login(s, username, passwd):
+    """Login to www.prosodie.com with username/passwd pair and return session.
+    Input:
+        s -- Requestium session (required |
+             type: requestium.requestium.Session);
+        username -- username on www.prosodie.com (required | type: str);
+        passwd -- password for username on www.prosodie.com (required |
+                  type: str).
+    Output:
+        s -- Requestium session (type: requestium.requestium.Session).
+
+    """
+
+    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
+          'EntryPoint?serviceName=LoginHandler'
+    s.driver.get(url)
+    s.driver.ensure_element_by_name('mail').send_keys(username)
+    s.driver.ensure_element_by_name('password').send_keys(passwd)
+    s.driver.ensure_element_by_name('valider').click()
+    return s
+
+def search_by_range(s, start_date=None, start_time=None, end_date=None,
+                    end_time=None):
+    """Search records on www.prosodie.com by date range and return session.
+    Input:
+        s -- Requestium session (required |
+             type: requestium.requestium.Session);
+        start_date -- start date (not required | type: str). Format:
+                      'mm:dd:yyyy'. Example: '03-05-1991';
+        start_time -- start time (not required | type: str). Example:
+                      '12:00 AM';
+        end_date -- end date (not required | type: str). Format:
+                    'mm:dd:yyyy'. Example: '03-05-1991';
+        end_time -- end time (not required | type: str). Example: '12:00 PM'.
+    Output:
+        s -- Requestium session (type: requestium.requestium.Session).
+
+    """
+
+    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
+          'EntryPoint?serviceName=CriteresMessagesHandler&lang=en'
+    s.driver.get(url)
+    if start_date:
+        s.driver.ensure_element_by_name('dateDebut').send_keys(start_date)
+    if start_time:
+        s.driver.ensure_element_by_name('heureDebut').send_keys(start_time)
+    if end_date:
+        s.driver.ensure_element_by_name('dateFin').send_keys(end_date)
+    if end_time:
+        s.driver.ensure_element_by_name('heureFin').send_keys(end_time)
+    s.driver.ensure_element_by_id('button-1009').click()
+    return s
+
+def search_by_ref(s, ref):
+    """Search record on www.prosodie.com by ref number and return session.
+    Input:
+        s -- Requestium session (required |
+             type: requestium.requestium.Session);
+        ref -- ref number (required | type: str). Example: '3905beTOd10339'.
+    Output:
+        s -- Requestium session (type: requestium.requestium.Session).
+
+    """
+
+    url = 'https://enregistreur.prosodie.com/odigo4isRecorder/' \
+          'EntryPoint?serviceName=CriteresMessagesHandler&lang=en'
+    s.driver.get(url)
+    s.driver.ensure_element_by_name('refEr').send_keys(ref)
+    s.driver.ensure_element_by_id('button-1009').click()
+    return s
+
+# Example. Download .MP3 file from www.prosodie.com by '3905beTOd10339'
+# ref number
+#download_mp3_by_ref(s, username, passwd, '3905beTOd10339')
+
+# Example. Download .MP3 file from www.prosodie.com by '3905beTOd10339'
+# ref number as /tmp/odigo.mp3
+#download_mp3_by_ref(s, username, passwd, '3905beTOd10339', '/tmp/odigo.mp3')
