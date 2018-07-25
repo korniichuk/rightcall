41c1ddc4a7d6b4a72901d24d3755db888555fcd8
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-07-25 14:34:01 +0200

    [odigo-auditor][comprehend] Add get_sentiment() function

--- comprehend.py
+++ comprehend.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import json
-
 import boto3
 
-bucket_name = 'transcribe.odigo-auditor'
+def get_sentiment(text, language_code='en'):
+    """Inspects text and returns an inference of the prevailing sentiment
+    (positive, neutral, mixed, or negative).
+    Input:
+        text -- UTF-8 text string. Each string must contain fewer that
+                5,000 bytes of UTF-8 encoded characters (required | type: str);
+        language_code -- language of text (not required | type: str |
+                         default: 'en').
+    Output:
+        sentiment -- sentiment: positive, neutral, mixed, or negative
+                     (type: str).
 
-comprehend = boto3.client('comprehend')
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    if bucket.name == bucket_name:
-        for key in bucket.objects.all():
-            if key.key.endswith('.json'):
-                result = {}
-                # Download json file
-                try:
-                    s3.Bucket(bucket_name).download_file(key.key, key.key)
-                except Exception as exception:
-                    #return 1
-                    pass
-                # Get text
-                with open(key.key, 'r') as f:
-                    data = json.load(f)
-                text = data['results']['transcripts'][0]['transcript']
-                result['text'] = text
-                # Get sentiment
-                r = comprehend.detect_sentiment(Text=text, LanguageCode='en')
-                sentiment = r['Sentiment'].lower()
-                result['sentiment'] = sentiment
+    """
+
+    comprehend = boto3.client('comprehend')
+    try:
+        r = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+    except Exception as exception:
+        return 1
+    sentiment = r['Sentiment'].lower()
+    return sentiment
+
+# Example. Get sentiment of text below:
+# "I ordered a small and expected it to fit just right but it was a little bit
+# more like a medium-large. It was great quality. It's a lighter brown than
+# pictured but fairly close. Would be ten times better if it was lined with
+# cotton or wool on the inside."
+#text = "I ordered a small and expected it to fit just right but it was a \
+#        little bit more like a medium-large. It was great quality. It's a \
+#        lighter brown than pictured but fairly close. Would be ten times \
+#        better if it was lined with cotton or wool on the inside."
+#get_sentiment(text)
