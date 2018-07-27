0f56449182c64ca7f80166857b03c46fb0d2fa19
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-07-27 16:36:42 +0200

    [odigo-auditor][main] Save result to Google Sheets with sheets.append_row() function

--- odigo-auditor.py
+++ odigo-auditor.py
@@ -9,40 +9,54 @@
 
 import json
 from os import remove
-from os.path import basename
+from os.path import basename, join
 
 import boto3
 
 from comprehend import get_sentiment
+from sheets import append_row
 
-bucket_name = 'transcribe.odigo-auditor'
+transcribe_bucket_name = 'transcribe.odigo-auditor'
+mp3_bucket_name = 'mp3.odigo-auditor'
 
-def main(bucket_name):
+def main(transcribe_bucket_name, mp3_bucket_name):
     """Auditor for Odigo tool"""
 
-    result = {}
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
-        if bucket.name == bucket_name:
-            i = 0
+        if bucket.name == transcribe_bucket_name:
             for key in bucket.objects.all():
                 if key.key.endswith('.json'):
-                    result[i] = {}
+                    r = {}
+                    # Get reference number
                     reference = basename(key.key).replace('.json', '')
-                    result[i]['reference'] = reference
+                    r['ref'] = reference
+                    # Get URL
+                    location = boto3.client('s3') \
+                            .get_bucket_location(
+                            Bucket=mp3_bucket_name)['LocationConstraint']
+                    base_url = join('https://s3-%s.amazonaws.com' % location,
+                            mp3_bucket_name)
+                    url = join(base_url, key.key.replace('.json', '.mp3'))
+                    r['url'] = url
                     # Download json file
                     try:
-                        s3.Bucket(bucket_name).download_file(key.key, key.key)
+                        s3.Bucket(transcribe_bucket_name) \
+                          .download_file(key.key, key.key)
                     except Exception as exception:
                         return 1
                     # Get text
                     with open(key.key, 'r') as f:
                         data = json.load(f)
                     text = data['results']['transcripts'][0]['transcript']
-                    result[i]['text'] = text
+                    r['text'] = text
                     # Get sentiment
                     sentiment = get_sentiment(text)
-                    result[i]['sentiment'] = sentiment
-                    # Remove json file
+                    r['sentiment'] = sentiment
+                    # Save to Gooogle Sheets
+                    values = [r['ref'], r['text'], r['sentiment'], r['url']]
+                    append_row(values)
+                    # Remove tmp json file from local machine
                     remove(key.key)
-                    i += 1
+
+main(transcribe_bucket_name, mp3_bucket_name)
